39f51d5c0260407f133e899dfaff588672e27007
Turith <christian.kronemberg@sempreceub.com>
2021-10-24 20:25:50 -0300

    getting project in git

---
+++ main.js
@@ -0,0 +1,33 @@
+
+            const LinksSocialMedia = {
+                github: "Turith",
+                youtube: "404",
+                facebook: "noneExistent",
+                instagram: "404",
+                twitter: "404"
+            }
+
+            function changeSocialMediaLinks(){
+                for(let li of socialLinks.children){ 
+                    const social = li.getAttribute('class')
+                    li.children[0].href = `https://${social}.com/${LinksSocialMedia[social]}`
+                }
+            }
+
+            changeSocialMediaLinks()
+
+            function getGitHubInfo(){
+                const url = `https://api.github.com/users/${LinksSocialMedia.github}`
+
+                fetch(url)
+                .then(response => response.json())
+                .then(data => {
+                    userName.textContent = data.name
+                    userBio.textContent = data.bio
+                    userLink.href = data.html_url
+                    userPhoto.src = data.avatar_url
+                    userLogin.textContent = data.login
+                })
+            }
+
+            getGitHubInfo()
